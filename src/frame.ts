de92e8687a4cb05a8c93d16fbc0a848abaccee9b
Adrian Scheit <adrianscheit@gmail.com>
2025-02-06 23:58:45 +0100

    Minor improvement

--- src/frame.ts
+++ src/frame.ts
@@ -33,7 +33,7 @@ export class Frame {
 
     getRow(): TableDataColumn[] {
         return [
-            new TableDataColumn(`${Frame.getDateTime()} (${this.type})`, this.type),
+            new TableDataColumn(`${Frame.getDateTime()} ${this.type}`, this.type),
             new TableDataColumn(`${this.slaveAddress} = 0x${Converters.byteToHex(this.slaveAddress!)}`, this.type),
             new TableDataColumn(FunctionCodes.getDescription(this.functionCode), FunctionCodes.isError(this.functionCode || 0) ? 'error' : this.type),
             new TableDataColumn(this.getDataLength().toString(), this.type),
